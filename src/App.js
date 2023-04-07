7c07c5a7153cb6842ef387b4f0d9ecab2eb00b21
Sublimeful <Qiangwu2000@gmail.com>
2023-04-06 20:44:23 -0500

    Added a bunch of stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import logo from './logo.svg';
-import './App.css';
-import TicTacToe from './components/tictactoe/TicTacToe.js'
+import "./App.css"
+import HomePage from './pages/homepage/HomePage.js';
 
-function MyButton({ count, onClick }) {
-	return (
-		<button type='button' onClick={onClick}>Hi, my number is {count}</button>
-	);
-}
 
 function App() {
-  let [ myCounter, setCounter ] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <TicTacToe />
-      </header>
+    <div className='App-header'>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,13 @@
+// Standard libraries
+import NavBar from "components/navbar/NavBar.js"
+
+
+export default function HomePage() {
+  return (
+    <div className="home-page">
+      <NavBar />
+    </div>
+  );
+}
+
+
