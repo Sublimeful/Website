b7f5e3f7f517e2cb8292055d853fcea8154f0d03
Sublimeful <Qiangwu2000@gmail.com>
2023-04-14 09:29:13 -0500

    Added all the pages!

---
+++ src/pages/about/About.js
@@ -0,0 +1,10 @@
+import classes from "./About.module.scss"
+
+export default function() {
+  return (
+    <main className={classes["page"]}>
+      <h1>ABOUT ME</h1>
+      <p>I have nothing so far here...</p>
+    </main>
+  );
+}
---
+++ src/pages/projects/Projects.js
@@ -0,0 +1,10 @@
+import classes from "./Projects.module.scss"
+
+export default function() {
+  return (
+    <main className={classes["page"]}>
+      <h1>UNDER CONSTRUCTION</h1>
+      <h2>This will contain a list of all my projects</h2>
+    </main>
+  );
+}
