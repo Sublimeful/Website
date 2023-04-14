3ae550ca976c9950d15477bef027c447410c6ea5
Sublimeful <Qiangwu2000@gmail.com>
2023-04-14 09:29:06 -0500

    Added all the pages!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import classes from "./App.module.scss"
 import HomePage from "pages/homepage/HomePage";
 import TicTacToe from "pages/projects/tictactoe/TicTacToe";
+import Projects from "pages/projects/Projects";
+import About from "pages/about/About";
 import PageContext from "contexts/PageContext";
 import NavBar from "components/navbar/NavBar";
 import { useState } from "react";
@@ -17,8 +19,12 @@ export default function() {
         <div className={classes["page"]}>
           {(() => {
             switch(currentPage) {
+              case "Projects":
+                return <Projects />
               case "TicTacToe":
                 return <TicTacToe />
+              case "About":
+                return <About />
               case "HomePage":
               default:
                 return <HomePage />
--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -3,6 +3,7 @@ import classes from "./HomePage.module.scss"
 export default function() {
   return (
     <main className={classes["page"]}>
+      <h1>Welcome to the Homepage!</h1>
     </main>
   );
 }
