b2a0973f549c441b4524c72654c9788101685763
Sublimeful <Qiangwu2000@gmail.com>
2023-04-14 09:40:34 -0500

    Started on Pong game

--- src/pages/projects/tictactoe/TicTacToe.js
+++ src/pages/projects/tictactoe/TicTacToe.js
@@ -4,6 +4,7 @@ import TicTacToe from "components/tictactoe/TicTacToe";
 export default function() {
   return (
     <main className={classes["page"]}>
+      <h1>Created: Apr 5, 2023</h1>
       <TicTacToe />
     </main>
   );
